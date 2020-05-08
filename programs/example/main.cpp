da804ac0943f89019b184616b5ab4dc89e2f3b89
a42dab9659090cac <a42dab9659090cac@outlook.com>
2020-05-08 11:41:27 +0800

    add http_plugin

--- programs/example/main.cpp
+++ programs/example/main.cpp
@@ -1,19 +1,36 @@
 #include <appbase/application.hpp>
+
+#include <potato/http_plugin/http_plugin.hpp>
+
 #include <iostream>
 #include <boost/exception/diagnostic_information.hpp>
 #include <fc/filesystem.hpp>
 #include <fc/exception/exception.hpp>
 
+using namespace appbase;
+using namespace potato;
+
 int main(int argc, char **argv) {
     try {
-        std::cout << u8"example " << appbase::app().version_string() << std::endl;
+        std::cout << u8"example " << app().version_string() << std::endl;
 
         auto root = fc::app_path();
 
-        if (!appbase::app().initialize(argc, argv))
+        http_plugin::set_defaults({
+                .default_unix_socket_path = "",
+                .default_http_port = 8888
+            });
+        if (!app().initialize(argc, argv))
+        {
+            const auto& opts = app().get_options();
+            if( opts.count("help") || opts.count("version") || opts.count("full-version") || opts.count("print-default-config") ) {
+                return 0;
+            }
             return -1;
-        appbase::app().startup();
-        appbase::app().exec();
+        }
+        app().startup();
+        app().set_thread_priority_max();
+        app().exec();
     } catch( const fc::exception& e ) {
         std::cerr << e.to_detail_string() << "\n";
     } catch( const boost::interprocess::bad_alloc& e ) {
